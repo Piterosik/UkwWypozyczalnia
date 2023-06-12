19bf6c7cd142966bacee5d5f1c10b46ecd543588
Piotr Januszewski <piotr.januszewski@codeagency.pl>
2023-06-13 00:03:47 +0200

    Dodano wyświetlanie filmów wg. wybranej kategorii

---
+++ ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using UkwWypozyczalnia.DAL;
+
+namespace UkwWypozyczalnia.ViewComponents
+{
+	public class MenuViewComponent : ViewComponent
+	{
+		FilmsContext _db;
+
+		public MenuViewComponent(FilmsContext db)
+		{
+			_db = db;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return await Task.FromResult((IViewComponentResult) View("_Menu", _db.Categories.ToList()));
+		}
+	}
+}
---
+++ Views/Films/FilmsList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UkwWypozyczalnia.Views.Films
+{
+    public class FilmsListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
