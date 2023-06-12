79fa88518f2aa7417f1930817fd2f69f02cae41b
Piotr Januszewski <piotr.januszewski@codeagency.pl>
2023-06-12 21:07:24 +0200

    Dodano modele

---
+++ Models/Film.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UkwWypozyczalnia.Models
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Brak tytułu!")]
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public float Price { get; set; }
+        public int CategoryId { get; set; }
+
+        public Category Category { get; set; }
+    }
+}
