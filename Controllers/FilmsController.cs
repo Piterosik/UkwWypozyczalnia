8b14fb7b2ba31e3740f5b18fd575a78ede3bd63d
Piotr Januszewski <piotr.januszewski@codeagency.pl>
2023-06-13 00:46:29 +0200

    Dodano operacje CRU do kontrolera filmów

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.EntityFrameworkCore;
 using System.Linq;
 using UkwWypozyczalnia.DAL;
+using UkwWypozyczalnia.Models;
+using UkwWypozyczalnia.ViewModels;
 
 namespace UkwWypozyczalnia.Controllers
 {
@@ -20,5 +22,49 @@ namespace UkwWypozyczalnia.Controllers
             var films = category.Films.ToList();
             return View(films);
         }
+
+        public IActionResult AddFilm()
+        {
+            var model = new AddFilmViewModel();
+            var categories = _db.Categories.ToList();
+
+            model.Categories = categories;
+
+            return View(model);
+        }
+
+        public IActionResult AddFilm(AddFilmViewModel model)
+        {
+            _db.Films.Add(model.Film);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult EditFilm(int id)
+        {
+            var film = _db.Films.Where(x => x.Id == id).FirstOrDefault();
+
+            return View(film);
+        }
+
+        public IActionResult EditFilm(Film film)
+        {
+            var dbFilm = _db.Films.Where(x => x.Id == film.Id).FirstOrDefault();
+
+            dbFilm.Title = film.Title;
+            dbFilm.Director = film.Director;
+            dbFilm.Price = film.Price;
+            dbFilm.CategoryId = film.CategoryId;
+
+            _db.Entry(dbFilm).State = EntityState.Modified;
+            _db.SaveChanges();
+
+            return RedirectToAction("ListFilms", "Films");
+        }
+
+
+
+
     }
 }
---
+++ ViewModels/AddFilmViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UkwWypozyczalnia.Models;
+
+namespace UkwWypozyczalnia.ViewModels
+{
+    public class AddFilmViewModel
+    {
+        public Film Film { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
